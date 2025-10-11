182e4cc093a66e24b48ce90709c9371829f1b4cd
Teo Jia Ling <jayelleteo@gmail.com>
2025-10-11 19:20:49 +0800

    Update hdb_sl_oldmodel.py

--- hdb_sl_oldmodel.py
+++ hdb_sl_oldmodel.py
@@ -270,8 +270,7 @@ Welcome! Here are some of my personal websites and portfolio pages where you can
 
 # List of links
 links = {
-    "Personal Website 1": "https://jayelle0609.github.io/",
-    "Portfolio Website 2": "https://jayelle0609.github.io/",
+    "Portfolio Website": "https://jayelle0609.github.io/",
     "Tableau Visualizations": "https://public.tableau.com/app/profile/jialingteo/vizzes",
     "GitHub Projects & Code": "https://github.com/jayelle0609",
     "Specificity HDB Model App": "https://jayellehdbspecific.streamlit.app/",
